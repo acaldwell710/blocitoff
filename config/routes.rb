31d50a00ee7c17cca1ea7f6946d04234170f14c8
Amber Caldwell <acaldwell710@gmail.com>
2018-03-28 16:18:50 -0400

    updated user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:show]
+  
   get 'welcome/index'
-  root 'welcome#index'
+  root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
