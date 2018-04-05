12e26856fc4546b666c407c3e1314b68c0b18cf6
Amber Caldwell <acaldwell710@gmail.com>
2018-04-04 21:40:11 -0400

    made changes to db tables

--- db/migrate/20180328202227_create_items.rb
+++ db/migrate/20180328202227_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[5.1]
 
   def change
-    drop_table :items
     create_table :items do |t|
       t.string :name
       t.references :user, foreign_key: true
